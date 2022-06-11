91ad6459343a399cb0bc7f427c16f371ca50b654
Julio Felipe <julio@informach.com.br>
2022-06-11 20:38:00 -0300

    feat: add serializers

---
+++ users/serializers.py
@@ -0,0 +1,21 @@
+from rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
+
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'is_student', 'is_teacher')
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    is_student = serializers.BooleanField()
+    is_teacher = serializers.BooleanField()
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'is_student', 'is_teacher')
+
+
+
+
